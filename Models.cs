5e4f590a3abc3d41b68cc3574fff1750b864bfee
Ranjam al <ranjam.alee@ineo.fi>
2022-06-21 22:35:23 +0300

    Add Create, Get functions

---
+++ Models.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AFApp
+{
+    public class SportsIcon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Sports { get; set; }
+    }
+
+    public class SportsIconCreateModel
+    {
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Sports { get; set; }
+    }
+
+    public class SportsIconUpdateModel
+    {
+        public string NickName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Sports { get; set; }
+    }
+}
---
+++ SportsIconFunction.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AFApp
+{
+    public static class SportsIconFunctions
+    {
+
+        public static readonly List<SportsIcon> SportsIcons = new List<SportsIcon> {
+            new SportsIcon { Id=1, Name = "Muhammad Ali", NickName = "The people's Champion", DateOfBirth = DateTime.ParseExact("01/07/1942", "MM/dd/yyyy", null), Sports="Boxing" },
+            new SportsIcon { Id=2, Name = "Pele", DateOfBirth = DateTime.ParseExact("10/23/1940", "MM/dd/yyyy", null), Sports="Football" },
+            new SportsIcon { Id=3, Name = "Mike Tyson", NickName = "Iron Mike", DateOfBirth = DateTime.ParseExact("06/30/1966", "MM/dd/yyyy", null), Sports="Boxing" },
+        };
+
+        [FunctionName("CreateSportsIcon")]
+        public static async Task<IActionResult> CreateSportsIcon(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "sportsicon")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Creating Sports icon.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<SportsIconCreateModel>(requestBody);
+
+            var sIcon = new SportsIcon() { Name = data.Name, NickName = data.NickName, DateOfBirth = data.DateOfBirth, Sports = data.Sports };
+            SportsIcons.Add(sIcon);
+
+            string responseMessage = string.IsNullOrEmpty(requestBody)
+                ? "This HTTP triggered function executed successfully. Pass a details in request body for a personalized response."
+                : "This HTTP triggered function executed successfully. Details : " + sIcon;
+
+            return new OkObjectResult(sIcon);
+        }
+
+        [FunctionName("GetAllSportsIcons")]
+        public static IActionResult GetAllSportsIcons(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "sportsicon")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Getting all sports Icons");
+            return new OkObjectResult(SportsIcons);
+        }
+
+        [FunctionName("GetSportsIconById")]
+        public static IActionResult GetSportsIconById(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "sportsicon/{id}")] HttpRequest req,
+            ILogger log, int id)
+        {
+            log.LogInformation("Getting a single sports Icon");
+
+            var result = SportsIcons.Find(s => s.Id == id);
+            return new OkObjectResult(result);
+        }
+    }
+}
